bebf4562dfaca09864ae97eee6a98018321a5cc6
Arantxa Fischer <arantxafischerc21@gmail.com>
2026-04-14 19:20:55 -0400

    Feat persona

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -2,3 +2,23 @@ package model;
 
 public class Persona {
 }
+
+// Atributos Persona
+protected String nombre;
+
+
+//Constructor
+public Persona(String nombre){
+    this.nombre = nombre;
+}
+// Getter
+public String getNombre(){
+    return nombre;
+}
+
+// Método para mostrar información
+public void mostrarInformacion (){
+    System.out.println("Nombre: " + nombre);
+}
+
+
